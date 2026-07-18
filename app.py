d421b3da208798ba392edc5de06214603a148ee4
Sanja Rishi <rishisanja99@gmail.com>
2026-07-18 18:21:52 +0530

    Initial Flask application setup

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello from CI/CD Trial Project!"
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
